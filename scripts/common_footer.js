473fafcbc7a9a839692a2304abaab3ec38ffb72e
yao-charlie <charlieyao@gmail.com>
2020-10-20 01:03:26 -0700

    -Refactored common header and footer into JS calls. Began photography page including intialization of lightboxes. Updated links in nav hamburger.

---
+++ scripts/common_footer.js
@@ -0,0 +1,28 @@
+var codeBlock =
+
+'<ul class = "footer__links">'+
+  '<li class="footer__link">'+
+    '<a class="footer__link" href="#">'+
+      '<i class="fas fa-phone-square"></i>'+
+    '</a>'+
+  '</li>'+
+  '<li class="footer__link">'+
+    '<a class="footer__link" href="#">'+
+      '<i class="fas fa-envelope-square"></i>'+
+    '</a>'+
+  '</li>'+
+
+  '<li class="footer__socialM-links__item">'+
+    '<a class="footer__socialM-links__item" href="https://www.linkedin.com/in/charlie-yao-38756723/" target="_blank">'+
+      '<i class="fab fa-linkedin"></i>'+
+    '</a>'+
+  '</li>'+
+  '<li class="footer__socialM-links__item">'+
+    '<a class="footer__socialM-links__item" href="https://github.com/yao-charlie" target="_blank">'+
+      '<i class="fab fa-github"></i>'+
+    '</a>'+
+  '</li>'+
+'</ul>';
+
+
+document.getElementById("common__footer").innerHTML = codeBlock
---
+++ scripts/common_header.js
@@ -0,0 +1,22 @@
+var codeBlock =
+
+  '<div class= "container">'+
+
+  '<h1 class="logo"><a class="link_subtle" href="index.html">Charlie Yao</a></h1>'+
+
+  '<button class="menu-toggle" role="button">'+
+
+    '<div class="hamburger"></div>'+
+
+  '</button>'+
+  '<nav class="site-nav site-nav--closed">'+
+    '<ul>'+
+      '<li><a href="Index.html">Profile</a></li>'+
+      '<li><a href="#">Professional</a></li>'+
+      '<li><a href="Photography.html">Photography</a></li>'+
+      '<li><a href="#">Projects</a></li>'+
+    '</ul>'+
+  '</nav>';
+
+
+document.getElementById("common__header").innerHTML = codeBlock
